edb9bb8efbf2f0b21c7816c760ff2722376076e8
Agnaldo230 <79926170+Agnaldo230@users.noreply.github.com>
2021-03-02 13:35:20 -0300

    Adicionando arquivo inicial

---
+++ Calculadora simples.py
@@ -0,0 +1,11 @@
+n1 = int (input('Digite um valor: '))
+n2 = int (input('Digite outro valor: '))
+s = n1 + n2
+s1 = n1 / n2
+s2 = n1 * n2
+s3 = n1 - n2
+print(f'O resultado de {n1} Mais {n2} é igual a {s} ')
+print(f'O resultado de {n1} Dividido por  {n2} é igual a {s1} ')
+print(f'O resultado de {n1} Multiplicado por {n2} é igual a {s2} ')
+print(f'O resultado de {n1} Menos {n2} é igual a {s3} ')
+
